167825b4ab6e0ce4a7ec4ccb9e82bb5080286c32
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-05-01 11:34:48 +0200

    Update dichotomique_search.py

--- dichotomique_search.py
+++ dichotomique_search.py
@@ -1,46 +1,54 @@
-input_ = input()
-input_ = input_.split(',')
-research = input()
-research = str(research)
-if (research in input_) == False :
-    print('error')
-i = (len(input_))-1
+minput = input()
+minput = minput.split(',')
+item = input()
+item = str(item)
+if (item in minput) == False :
+    print('error : cannot find your item in your input')
+    print('input :', minput)
+    print('item :',item)
+    item = input()
+i = (len(minput))
 j = int(i/2)
-input_.sort()
-print(input_)
-result = j
-if input_[j] == research :
-    print('index',j+1)
-while input_[j] != research :
-        print('bip bidiboup')
-        j = int(len(input_)/2)
-        i = len(input_)-1
-        if input_[j] < research :
-            print(input_[j],'>',research)
+minput = sorted(minput, key=int)
+minput_uncut = list(minput)
+print(minput)
+index = j
+if minput[j] == item :
+    print('index ->',j)
+while str(minput_uncut[index]) != str(item) :
+        j = int(len(minput)/2)
+        i = len(minput)
+        if int(minput[j]) < int(item) :
             y = [*range(j+1)]
             y.sort(reverse=True)
-            print(y)
             z = 0
             while z < len(y) :
                 h = y[z]
-                del input_[h]
-                print('j',j,'i',i)
-                print('>',input_,y[z])
+                del minput[h]
                 z = z + 1
-            result = (result + (len(y)-1))+(len(input_)/2)
-        elif input_[j] > research :
-            print(input_[j],'>',research)
-            y = [*range(j,i+1)]
+            if (isinstance(index, float)) == True :
+                index = (index + len(minput)/2) + 0.5
+            else :
+                index = (index + len(minput)/2) +1
+                index = int(index)
+        elif int(minput[j]) > int(item) :
+            y = [*range(j,i)]
             y.sort(reverse=True)
-            print(y)
             z = 0
             while z < len(y) :
                 h = y[z]
-                del input_[h]
-                print('j',j,'i',i)
-                print('>',input_,y[z])
+                del minput[h]
                 z = z + 1
-            result = result
-        j = int(len(input_)/2)
-        i = len(input_)-1
-print('index',result)
+            if (isinstance(index, float)) == True :
+                index = (index - len(minput)/2) - 0.5
+            else :
+                index = (index - len(minput)/2)
+                index = int(index)
+        j = int(len(minput)/2)
+        i = len(minput)
+        print('>',minput)
+        if minput_uncut[int(index)] != minput[j] :
+            print('an error occured')
+if (minput_uncut[int(index)]) != item :
+    print('an error occured')
+print('index ->',index)
